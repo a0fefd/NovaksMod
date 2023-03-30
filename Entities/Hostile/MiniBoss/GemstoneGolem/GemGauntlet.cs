fe282703a2bc75fa29c2ce5ed6a7f75dddf7ec03
masternbslay <52446438+masternbslay@users.noreply.github.com>
2023-03-30 15:12:09 +1000

    Update

---
+++ Entities/Hostile/MiniBoss/GemstoneGolem/GemGauntlet.cs
@@ -0,0 +1,46 @@
+﻿using Humanizer;
+using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace NovaksMod.Entities.Hostile.MiniBoss.GemstoneGolem
+{
+    internal class GemGauntlet : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            Tooltip.SetDefault("Awakens the protecter of gemstones...\n" +
+                "Summons the Gemstone Golem.");
+        }
+        public override void SetDefaults()
+        {
+            Item.width = 26;
+            Item.height = 26;
+            Item.useTime = 15;
+            Item.useAnimation = 15;
+            Item.useStyle = ItemUseStyleID.RaiseLamp;
+            Item.UseSound = SoundID.Item2;
+            Item.maxStack = 30;
+            Item.consumable = true;
+        }
+        public override bool? UseItem(Player player)
+        {
+            NPC.NewNPC(player.GetSource_FromThis(), (int)player.itemLocation.X -15, (int)player.itemLocation.Y, ModContent.NPCType<GemstoneGolem>());
+            Main.NewText("Test");
+
+            return true;
+        }
+        public override bool CanUseItem(Player player)
+        {
+            // Only allow the player to use this item if they are missing health
+            return !Main.dayTime;
+        }
+    }
+}
--- Entities/Hostile/MiniBoss/GemstoneGolem/GemstoneGolem.cs
+++ Entities/Hostile/MiniBoss/GemstoneGolem/GemstoneGolem.cs
@@ -6,38 +6,30 @@ using System.Threading.Tasks;
 using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
-using Terraria.ModLoader.Utilities;
 
-namespace NovaksMod.Entities.Hostile.NonBoss
+namespace NovaksMod.Entities.Hostile.MiniBoss.GemstoneGolem
 {
-    internal class ChlorophyteSlime : ModNPC
+    internal class GemstoneGolem : ModNPC
     {
         public override void SetStaticDefaults()
         {
-            DisplayName.SetDefault("Clorophyte Slime");
             Main.npcFrameCount[NPC.type] = Main.npcFrameCount[2];
         }
         public override void SetDefaults()
         {
-            NPC.width = 32; NPC.height = 15;
-            NPC.damage = 25;
-            NPC.defense = 20;
-            NPC.lifeMax = 150;
+            NPC.width = 40; NPC.height = 50;
+            NPC.damage = 10;
+            NPC.defense = 5;
+            NPC.lifeMax = 700;
             NPC.value = Item.sellPrice(silver: 1);
-            NPC.aiStyle = 1;
+            NPC.aiStyle = NPCAIStyleID.Mimic;
             NPC.HitSound = SoundID.NPCHit1;
             NPC.DeathSound = SoundID.NPCDeath1;
             AIType = NPCID.GreenSlime;
-            AnimationType = NPCID.GreenSlime;
+            AnimationType = NPCID.Paladin;
             NPC.friendly = false;
-        }
-        public override float SpawnChance(NPCSpawnInfo spawnInfo)
-        {
-            if (spawnInfo.PlanteraDefeated)
-            {
-                return SpawnCondition.UndergroundJungle.Chance * 0.20f;
-            }
-            return 0f;
+            NPC.scale = 1;
+            NPC.boss = true;
         }
         public override void FindFrame(int frameHeight)
         {
@@ -50,8 +42,14 @@ namespace NovaksMod.Entities.Hostile.NonBoss
         }
         public override void OnKill()
         {
-            Item.NewItem(NPC.GetSource_Death(), NPC.getRect(), ItemID.Gel, Main.rand.Next(0, 2));
-            Item.NewItem(NPC.GetSource_Death(), NPC.getRect(), ItemID.ChlorophyteOre, Main.rand.Next(1, 5));
+            int s1 = Main.rand.Next(0, 6);
+            int s2 = Main.rand.Next(0, 6);
+            int s3 = Main.rand.Next(0, 6);
+            int[] gems = { ItemID.Topaz, ItemID.Diamond, ItemID.Sapphire, ItemID.Emerald, ItemID.Amethyst, ItemID.Amber, ItemID.Ruby };
+
+            Item.NewItem(NPC.GetSource_Death(), NPC.getRect(), gems[s1], Main.rand.Next(1, 3));
+            Item.NewItem(NPC.GetSource_Death(), NPC.getRect(), gems[s2], Main.rand.Next(1, 3));
+            Item.NewItem(NPC.GetSource_Death(), NPC.getRect(), gems[s3], Main.rand.Next(1, 3));
         }
     }
 }
